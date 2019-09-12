a0fcde8603851143f600bab80a44b016c1cd11a9
Charles Beach <charlesbeach@Charless-MacBook-Pro.local>
2019-09-12 14:52:17 -0400

    create a controller for Courses

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,53 @@
+class CoursesController < ApplicationController
+    # before_action: :set_course, only: %i[show update destroy]
+
+    def index
+        @user = User.find(params[:user_id])
+        @courses = Course.where(user_id: @user.id)
+        render json: @courses, status: :ok
+    end
+
+    def show
+        begin
+            @user = User.find(params[:user_id])
+            @course = Course.where(user_id: @user.id).find(params[:id])
+            render json: @course, status: :ok
+        rescue ActiveRecord::RecordNotFound
+            render json: { message: 'course not found for that user' }, status: 404
+        rescue StandardError => e
+            render json: { message: e.to_s }, status: 500
+        end
+    end
+
+    def create
+        @user = User.find(params[:user_id])
+        @course = Course.where(user_id: @user.id).new(course_params)
+        if @course.save
+            render json: @course, status: :created
+        else
+            render json: @course.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @user = User.find(params[:user_id])
+        @course = Course.where(user_id: @user.id).find(params[:id])
+        if @course.update(course_params)
+            render json: @course, status: :ok
+        else
+            render json: @course.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:user_id])
+        @course = Course.where(user_id: @user.id).find(params[:id])
+        @course.destroy
+    end
+
+    private
+
+    def course_params
+        params.permit(:name, :description)
+    end
+end
