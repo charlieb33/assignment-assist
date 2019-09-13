4ebaeb22d3bb60af9e9c2661791f42a0d975bf93
Charles Beach <charlesbeach@Charless-MacBook-Pro.local>
2019-09-12 21:47:54 -0400

    create a controller for authentication

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,17 @@
+class AuthenticationController < ApplicationController
+    def login
+        @user = User.find_by_username(params[:username])
+        if @user.authenticate(params[:password])
+            token = encode(user_id: @user.id, @user.username)
+            render json: { token: token }, status: :ok
+        else
+            render json { error: 'unauthorized '}, status: :unauthorized
+        end
+    end
+
+    private
+
+    def login_params
+        params.permit(:username, :password)
+    end
+end
