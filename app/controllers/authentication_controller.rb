90f74ba6d09fc2642c3267ea8ce0992c33456c4a
Charles Beach <charlesbeach@Charless-MacBook-Pro.local>
2019-09-13 13:49:11 -0400

    create a controller for Users, update code in ApplicationController, AuthenticationController, db/seeds.db, and config/routes.db

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,10 +2,10 @@ class AuthenticationController < ApplicationController
     def login
         @user = User.find_by_username(params[:username])
         if @user.authenticate(params[:password])
-            token = encode(user_id: @user.id, @user.username)
-            render json: { token: token }, status: :ok
+            token = encode(id: @user.id, username: @user.username)
+            render json: { token: token, user: { id: @user.id, username: @user.username } }, status: :ok
         else
-            render json { error: 'unauthorized '}, status: :unauthorized
+            render json: { error: 'unauthorized '}, status: :unauthorized
         end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   post '/auth/login', to: 'authentication#login'
-  
+  get '/users/verify', to: 'users#verify'
+
   resources :users do
     resources :courses do
       resources :assignments
