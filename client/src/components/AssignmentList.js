40b1a5c0232ad1cba93634af099254326ba5c366
Charles Beach <charlesbeach@Charless-MacBook-Pro.local>
2019-09-15 15:20:06 -0400

    add code to CourseList and AssignmentList components

---
+++ client/src/components/AssignmentList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { withRouter } from 'react-router';
+
+const AssignmentList = (props) => {
+    return(
+        <div className="assignment-list-container">
+            <div>
+                { props.assignments.map((assignment, course) => (
+                    <div
+                        key={assignment.id}
+                        className="assignment-card"
+                        onClick={() =>
+                            props.history.push(`/courses/${course.id}/assignments${assignment.id}`)
+                        }
+                    >
+                        <h3>{assignment.name}</h3>
+                    </div>
+                ))}
+            </div>
+            <div className="add-assignment">
+                <h3>Add an Assignment</h3>
+            </div>
+        </div>
+    );
+};
+
+export default withRouter(AssignmentList);
--- client/src/components/CourseList.js
+++ client/src/components/CourseList.js
@@ -1,2 +1,25 @@
 import React from 'react';
 import { withRouter } from 'react-router';
+
+const CourseList = (props) => {
+    return(
+        <div className="course-list-container">
+            <div>
+                { props.courses.map(course => (
+                    <div
+                        key={course.id}
+                        className="course-card"
+                        onClick={() => props.history.push(`/courses/${course.id}`)}
+                    >
+                        <h3>{course.name}</h3>
+                    </div>
+                ))}
+            </div>
+            <div className="add-course">
+                <h3>Add a Course</h3>
+            </div>
+        </div>
+    );
+};
+
+export default withRouter(CourseList);
