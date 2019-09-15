ef22ba766f9c393cc77d6531426172a10a4e41cd
Charles Beach <charlesbeach@Charless-MacBook-Pro.local>
2019-09-15 09:26:41 -0400

    update Home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,16 +4,21 @@ import { Link } from "react-router-dom";
 class Home extends Component {
     render() {
         return (
-            <div>
-                <p>A new and convenient way to keep track of your courses and assignments.
-                    Simply log in or register to begin. Once you are signed in, you will be
-                    able to access information on any courses you have, or you can add courses
-                    to the list. By clicking on a course, you will be able to see their name,
-                    a description on it, a list of assignments for that course, and the options
-                    to edit or delete that course or add a new assignment for it. Clicking on an
-                    assignment in the list will allow you to see its name, description, and due date,
-                    and allow access to editing or deleting that assignment.
-                </p>
+            <div className="home-container">
+                <div className="about-the-app">
+                    <p>A new and convenient way to keep track of your courses and assignments.
+                        Simply log in or register to begin. Once you are signed in, you will be
+                        able to access information on any courses you have, or you can add courses
+                        to the list. By clicking on a course, you will be able to see their name,
+                        a description on it, a list of assignments for that course, and the options
+                        to edit or delete that course or add a new assignment for it. Clicking on an
+                        assignment in the list will allow you to see its name, description, and due date,
+                        and allow access to editing or deleting that assignment.
+                    </p>
+                </div>
+                <div className="courses-container">
+                    <h2>Courses</h2>
+                </div>
             </div>
         );
     };
